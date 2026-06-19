9b5a7a12a36105d0375d5e4c22d2101b7ec2475b
24rtz9wywr-ui <24rtz9wywr@privaterelay.appleid.com>
2026-06-19 14:13:17 +0100

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,3 +1,29 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "API working" });
+export default async function handler(req, res) {
+
+  const company = req.query.company;
+
+  if (!company) {
+    return res.status(400).json({
+      error: "Missing company"
+    });
+  }
+
+  try {
+
+    const searchUrl =
+      `https://duckduckgo.com/?q=${encodeURIComponent(company)}`;
+
+    res.status(200).json({
+      company,
+      search: searchUrl
+    });
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
+
 }
